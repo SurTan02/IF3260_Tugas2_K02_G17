e1650517a97aa140705260b26a932e954a3ee4cd
adeliaaaa <monicadelia88@gmail.com>
2023-03-24 08:51:56 +0700

    refactor: update resetCamera & add yCamera slider

--- src/js/script.js
+++ src/js/script.js
@@ -6,8 +6,10 @@ const slider_ry = document.getElementById("ryField");
 const slider_rz = document.getElementById("rzField");
 const slider_sx = document.getElementById("sxField");
 const slider_sy = document.getElementById("syField");
+const slider_yc = document.getElementById("yCamera");
 const slider_sz = document.getElementById("szField");
 const slider_zc = document.getElementById("zoomCamera");
+
 const loader = document.getElementById("load");
 const resetCamera = document.getElementById("resetbutton");
 
@@ -20,6 +22,7 @@ var rz = 0;
 var sx = 1;
 var sy = 1;
 var sz = 1;
+var yc = 0;
 var zc = 90;
 
 slider_tx.value = tx;
@@ -31,6 +34,7 @@ slider_sz.value = sz;
 slider_rx.value = rx;
 slider_ry.value = ry;
 slider_rz.value = rz;
+slider_yc.value = yc;
 slider_zc.value = zc;
 
 slider_tx.oninput = function () {
@@ -69,6 +73,10 @@ slider_sz.oninput = function () {
 	sz = this.value;
 };
 
+slider_yc.oninput = function () {
+	yc = (this.value / 180) * Math.PI;
+};
+
 slider_zc.oninput = function () {
 	zc = this.value;
 };
@@ -88,19 +96,27 @@ loader.onchange = function (e) {
 };
 
 resetCamera.onclick = function (e) {
+	tx = 0;
+	ty = 0;
+	tz = 0;
 	rx = 0;
 	ry = 0;
 	rz = 0;
 	sx = 1;
 	sy = 1;
 	sz = 1;
+	yc = 0;
 	zc = 90;
 
+	slider_tx.value = tx;
+	slider_ty.value = ty;
+	slider_ty.value = ty;
 	slider_sx.value = sx;
 	slider_sy.value = sy;
 	slider_sz.value = sz;
 	slider_rx.value = rx;
 	slider_ry.value = ry;
 	slider_rz.value = rz;
+	slider_yc.value = yc;
 	slider_zc.value = zc;
 };
